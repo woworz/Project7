0730034ca69b409c0b1a916ba0b0da71bc256e01
woworz <masonjiang2003@outlook.com>
2022-05-04 22:18:39 +0800

    homework

--- src/com/pr7/GoodsShop.java
+++ src/com/pr7/GoodsShop.java
@@ -2,11 +2,12 @@ package com.pr7;
 
 public class GoodsShop {
     public double money = 0;
+    // 定义卖商品这个行为
     public double sellGoods(Goods goods) {
         double price = goods.getPrince();
-        return money+=price;
+        return money+=price; // 卖商品收入
     }
     public double getMoney() {
-        return money;
+        return money; // 最后返回总收入
     }
 }
--- src/com/pr7/Test.java
+++ src/com/pr7/Test.java
@@ -1,4 +1,17 @@
 package com.pr7;
 
 public class Test {
+    public static void main(String[] args) {
+        /*
+        * 将卖商品和商品分开
+        * 当需要添加商品时，只需要添加商品类即可，无需更改GoodsShop
+         */
+        GoodsShop shop = new GoodsShop();
+        Goods goods1 = new Cola();
+        Goods goods2 = new Fanta();
+        shop.sellGoods(goods1);
+        shop.sellGoods(goods2);
+        double money = shop.getMoney();
+        System.out.println("一共售出价格："+money);
+    }
 }
